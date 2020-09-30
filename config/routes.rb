96f518fa6d6067da058e2b37123f946c5bded187
Kibayashi <kentaro.kibayashi@gmail.com>
2020-09-30 18:43:04 +0900

    コントローラー追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'reviews/index'
+  get 'reviews/show'
+  get 'reviews/new'
+  get 'reviews/edit'
   devise_for :users
   root to: "home#index"
   resources :users
