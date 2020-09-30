d8f810b8dee91aaeb3a2319dda90ab5d047b5c39
Kibayashi <kentaro.kibayashi@gmail.com>
2020-09-30 10:57:25 +0900

    RuntimeError 解決

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'cf5cd93d30e4505fa0eb5682c67f39f2f253784a5495da416045124c7ae257c8f866486a23434db634cbf3c6a63289c2dbdde153c73df92755a5db3a67915f33'
