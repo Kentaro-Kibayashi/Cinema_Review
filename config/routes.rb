9de45d4269c3c3d498ce9e6f72931b2a1ddfd300
Kibayashi <kentaro.kibayashi@gmail.com>
2020-09-30 18:45:13 +0900

    routes変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'reviews/index'
-  get 'reviews/show'
-  get 'reviews/new'
-  get 'reviews/edit'
   devise_for :users
   root to: "home#index"
   resources :users
+  resources :reviews
 end
