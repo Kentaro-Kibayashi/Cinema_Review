40d9c29524fe35261889d302c32a8c34e922f295
Kibayashi <kentaro.kibayashi@gmail.com>
2020-09-30 20:11:18 +0900

    各アクション編集

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,12 +3,26 @@ class ReviewsController < ApplicationController
   end
 
   def show
+    @review = Review.find(params[:id])
   end
 
   def new
     @review = Review.new
   end
 
+  def create
+    @review = Review.new(review_params)
+    @review.user_id = current_user.id
+    @review.save
+    redirect_to review_path(@review)
+  end
+
   def edit
   end
+
+  private
+  def review_params
+    params.require(:review).permit(:title, :body, :image)
+  end
+
 end
