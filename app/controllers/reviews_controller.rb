ee031e3aaa04e529744ea3a6232a1b9d2316d89f
Kibayashi <kentaro.kibayashi@gmail.com>
2020-10-01 19:44:59 +0900

    validation 編集

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -16,18 +16,27 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     @review.user_id = current_user.id
-    @review.save
-    redirect_to review_path(@review)
+    if @review.save
+      redirect_to review_path(@review), notice: '投稿に成功しました！'
+    else
+      render :new
+    end
   end
 
   def edit
     @review = Review.find(params[:id])
+    if @review.user != current_user
+      redirect_to reviews_path, alert: '不正なアクセスです！'
+    end
   end
 
   def update
     @review = Review.find(params[:id])
-    @review.update(review_params)
-    redirect_to review_path(@review)
+    if @review.update(review_params)
+      redirect_to review_path(@review), notice: '更新に成功しました！'
+    else
+      render :edit
+    end
   end
 
   def destroy
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,11 @@
 class Review < ApplicationRecord
   belongs_to :user
   attachment :image
+
+  with_options presence: true do
+    validates :title
+    validates :body
+    validates :image
+  end
+  
 end
