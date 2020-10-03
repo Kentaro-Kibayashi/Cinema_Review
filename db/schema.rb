f0617f48c2dcda6dec4b07ff076f7f3134c13bcc
Kibayashi <kentaro.kibayashi@gmail.com>
2020-10-03 11:41:09 +0900

    いいね機能実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_30_093828) do
+ActiveRecord::Schema.define(version: 2020_10_02_053312) do
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "review_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "reviews", force: :cascade do |t|
     t.integer "user_id"
