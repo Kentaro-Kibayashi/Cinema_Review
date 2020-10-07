d6946e280e492a6855b7c7f858d0c95b22d42d56
Kibayashi <kentaro.kibayashi@gmail.com>
2020-10-07 22:04:52 +0900

    ゲストログイン機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
   resources :users
+  devise_scope :user do
+    post '/users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
   resources :reviews do
     resource :favorites, only: [:create, :destroy]
   end
