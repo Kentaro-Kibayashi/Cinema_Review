167ec79f05aa6b549f9c18a4e29b795f40f5c4cd
Kibayashi <kentaro.kibayashi@gmail.com>
2020-10-08 18:21:31 +0900

    s3設定

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,12 +5,12 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
   config.storage :fog
   config.fog_provider = 'fog/aws'
-  config.fog_directory  = 'cinema-review-s3'
+  config.fog_directory  = ENV['AWS_S3_BUCKET']
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: ENV['AKIA6G6D3GRBEHX6X4XN'],
-    aws_secret_access_key: ENV['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'],
-    region: ENV['ap-northeast-1'],
+    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    region: ENV['AWS_S3_REGION'],
     path_style: true
   }
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_02_053312) do
+ActiveRecord::Schema.define(version: 2020_10_03_034924) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2020_10_02_053312) do
     t.string "image_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "score"
   end
 
   create_table "users", force: :cascade do |t|
