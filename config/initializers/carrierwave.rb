0a6746ec746a4746d8545c255298024135977ade
Kibayashi <kentaro.kibayashi@gmail.com>
2020-10-08 19:21:02 +0900

    修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,6 +6,7 @@ CarrierWave.configure do |config|
   config.storage :fog
   config.fog_provider = 'fog/aws'
   config.fog_directory  = ENV['AWS_S3_BUCKET']
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/cinema-review-s3'
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
